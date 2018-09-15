b8c3e98c487ec1d82f1e64fbe8252121226ecc5b
Ryan Nourbaran <arya.nourbaran@hotmail.com>
2018-09-15 14:25:53 -0400

    added flaskRestAPI

---
+++ flaskRestAPI/application.py
@@ -0,0 +1,26 @@
+from flask import Flask, url_for
+from flask import request
+
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+@app.route('/xax')
+def api_root():
+    return 'Welcome'
+
+
+@app.route('/messages', methods=['POST'])
+def api_messages():
+    if 'audience_image' in request.files:
+        f = request.files['audience_image']
+        f.save("aaa"+f.filename)
+        return "file uploaded"
+
+    else:
+        return "Error has occured"
+
+
+if __name__ == '__main__':
+    app.run()
