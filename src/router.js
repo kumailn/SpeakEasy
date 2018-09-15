0958112c4cfc78616620741ba951e517a3a54efc
KevinTXY <kevin@DESKTOP-37O70HU.localdomain>
2018-09-15 14:59:38 -0400

    login page designed

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import History from './views/History.vue';
+import Login from './views/Login.vue';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/history',
       name: 'history',
       component: History,
+    },
+    {
+      path:'/login',
+      name: 'login',
+      component: Login,
     },
   ],
 });
