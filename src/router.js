bba90ff85a807844246145b42ee06b1c690b2e39
KevinTXY <kevin@DESKTOP-37O70HU.localdomain>
2018-09-15 14:04:57 -0400

    history page added

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import History from './views/History.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  
   routes: [
     {
       path: '/',
@@ -12,12 +15,9 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/history',
+      name: 'history',
+      component: History,
     },
   ],
 });
---
+++ src/views/History.vue
@@ -0,0 +1,176 @@
+<template>
+  <v-container fluid>
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card>
+            <apexcharts width="100%" type="area" :options="options" :series="series" height="400px"></apexcharts>
+          </v-card>
+        </v-flex>
+        <v-flex xs4>
+          <v-card dark color="secondary">
+            <div id="container">
+              <video autoplay="true" id="videoElement" style="width: 100%; height: 100%;" />
+            </div>
+          </v-card>
+        </v-flex>
+        <v-flex v-for="i in 2" :key="`4${i}`" xs4>
+          <v-card>
+            <v-card-text class="px-0">
+              <apexcharts width="100%" type="radialBar" :options="emotionBreakdown" :series="emotionBreakdown.series" height="390px" />
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex v-for="i in 4 " :key="`3${i}` " xs3>
+          <v-card dark color="secondary ">
+            <v-card-text class="px-0 ">3</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex v-for="i in 6 " :key="`2${i}` " xs2>
+          <v-card dark color="primary ">
+            <v-card-text class="px-0 ">2</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex v-for="i in 12 " :key="`1${i}` " xs1>
+          <v-card dark color="secondary ">
+            <v-card-text class="px-0 ">1</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-slide-y-transition mode="out-in ">
+
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script>
+import VueApexCharts from 'vue-apexcharts';
+import db from '../private';
+export default {
+    components: {
+        apexcharts: VueApexCharts,
+    },
+    data() {
+        return {
+            emotionBreakdown: {
+                chart: {
+                    height: 380,
+                    type: 'radialBar',
+                    animations: {
+                        enabled: true,
+                        easing: 'easeinout',
+                        speed: 800,
+                        animateGradually: {
+                            enabled: true,
+                            delay: 300,
+                        },
+                        dynamicAnimation: {
+                            enabled: false,
+                            speed: 500,
+                        },
+                    },
+                },
+                plotOptions: {
+                    circle: {
+                        dataLabels: {
+                            showOn: 'hover',
+                        },
+                    },
+                },
+                series: [],
+                labels: [],
+            },
+            options: {
+                chart: {
+                    id: 'vuechart-example',
+                },
+                xaxis: {
+                    categories: [
+                        1991,
+                        1992,
+                        1993,
+                        1994,
+                        1995,
+                        1996,
+                        1997,
+                        1998,
+                        423,
+                        543,
+                        653,
+                        6543,
+                        645,
+                        234,
+                    ],
+                },
+            },
+            series: [
+                {
+                    name: 'series-1',
+                    data: [30, 40, 45, 50, 49, 60, 70, 91, 34, 53, 54, 77, 33, 11, 88, 66],
+                },
+            ],
+        };
+    },
+    async beforeMount() {
+        let res = await db
+            .collection('sessions')
+            .doc('1')
+            .get();
+        console.log(res.data());
+        this.emotionBreakdown.series = res.data().chartData.emotionBreakdown.series;
+        this.emotionBreakdown.labels = res.data().chartData.emotionBreakdown.labels;
+    },
+    mounted() {
+        const video = document.querySelector('#videoElement');
+
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices
+                .getUserMedia({ video: true })
+                .then(stream => {
+                    video.srcObject = stream;
+                })
+                .catch(err0r => {
+                    console.log('Something went wrong!');
+                });
+        }
+        // window.setInterval(this.addData(), 1000);
+    },
+    methods: {
+        addData() {
+            this.options.xaxis.categories.push(Math.floor(Math.random() * 100));
+            this.series[0].data.push(Math.floor(Math.random() * 100));
+
+            this.series[0].data.shift();
+            this.options.xaxis.categories.shift();
+        },
+        updateEmotionBreakdown() {
+            const n = [];
+            for (let i = 0; i < 4; i++) {
+                n.push(Math.floor(Math.random() * 100));
+            }
+            this.emotionBreakdown.series = n;
+        },
+    },
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+    font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
